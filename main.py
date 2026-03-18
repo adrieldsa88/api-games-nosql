09af3698bfdf910b4ab7dc4ee50f555744227f34
Adriel Silva <adrieldsa88@gmail.com>
2026-03-18 09:32:38 -0300

    fix main

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ app = FastAPI(
     version="2.0.0"
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(router_usuarios)
 app.include_router(router_jogos)
 app.include_router(router_avaliacoes)
